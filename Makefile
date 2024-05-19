cd7ca22f0870e2f5c408dec99555fc5bfdf70586
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-05-19 20:38:17 +0200

    cli

---
+++ Makefile
@@ -0,0 +1,43 @@
+sources = skboilerplate tests
+
+clean-folders:
+	rm -rf __pycache__ */__pycache__ */**/__pycache__ \
+		.pytest_cache */.pytest_cache */**/.pytest_cache \
+		.ruff_cache */.ruff_cache */**/.ruff_cache \
+		.mypy_cache */.mypy_cache */**/.mypy_cache \
+		site build dist htmlcov .coverage .tox
+
+lint:
+	ruff version
+	ruff format $(sources)
+	ruff check $(sources) --fix
+	ruff clean
+
+test:
+	pytest tests -n auto
+
+coverage:
+	rm -rf .coverage
+	(rm docs/img/coverage.svg) || (echo "No coverage.svg file found")
+	coverage run -m pytest
+	coverage report -m
+	coverage-badge -o docs/img/coverage.svg
+
+types:
+	mypy $(sources)
+
+check: lint test types clean-folders
+
+docs-serve:
+	mkdocs serve
+
+docs-deploy:
+	mkdocs gh-deploy
+
+pypi-push:
+	rm -rf dist
+	hatch build
+	hatch publish
+
+get-version :
+	@echo $(shell grep -m 1 version pyproject.toml | tr -s ' ' | tr -d '"' | tr -d "'" | cut -d' ' -f3)
--- skboilerplate/__main__.py
+++ skboilerplate/__main__.py
@@ -1,23 +1,61 @@
-from jinja2 import Template
-from typer import Typer, Option
+import subprocess
+from enum import Enum
 from importlib import resources
 from pathlib import Path
+from typing import Final, Union
 
-file_path: Path = resources.files("skboilerplate") / "template.py.jinja"  # type: ignore
-
-with open(file_path, "r") as stream:
-    template = Template(stream.read())
+import typer
+from jinja2 import Template
 
 
-values = {
-    "name": "RandomRegressor",
-    "regressor": True,
-    "classifier": True,
-    "linear": True,
-    "support_sample_weight": True,
-    "required": ["k"],
-    "parameters": ["k", "alpha", "beta", "max_iter"],
-}
+class EstimatorType(str, Enum):
+    """List of possible estimator types."""
 
-with open(f"xyz.py", "w") as destination:
-    destination.write(template.render(values))
+    ClassifierMixin = "classifier"
+    OutlierMixin = "outlier"
+    RegressorMixin = "regressor"
+    TransformerMixin = "transformer"
+
+
+TEMPLATE_PATH: Final[Path] = resources.files("skboilerplate") / "template.py.jinja"
+
+
+def main(
+    name: str,
+    estimator_type: EstimatorType,
+    required_params: Union[str, None] = None,  # noqa: UP007
+    other_params: Union[str, None] = None,  # noqa: UP007
+    support_sample_weight: bool = False,
+    linear: bool = False,
+    output_file: str = "",
+) -> None:
+    """Generate boilerplate code given options."""
+    with TEMPLATE_PATH.open(mode="r") as stream:
+        template = Template(stream.read())
+
+    required = required_params.split(",") if required_params else []
+    others = other_params.split(",") if other_params else []
+    params = [*required, *others]
+
+    values = {
+        "name": name,
+        "estimator_type": estimator_type.value,
+        "mixin": estimator_type.name,
+        "linear": linear,
+        "support_sample_weight": support_sample_weight,
+        "required": required,
+        "others": others,
+        "parameters": params,
+    }
+
+    destination_file = Path(output_file or f"{name.lower()}.py")
+    destination_file.parent.mkdir(parents=True, exist_ok=True)
+
+    with destination_file.open(mode="w") as destination:
+        destination.write(template.render(values))
+
+    subprocess.run(["ruff", "format", str(destination_file), "-q"], check=True)
+
+
+if __name__ == "__main__":
+    typer.run(main)
