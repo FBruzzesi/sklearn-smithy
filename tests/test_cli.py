747218ce2ef9ab688bb8c30805c95f402687c447
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2024-06-06 22:43:14 +0200

    100% on cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,7 +10,12 @@ from sksmithy._prompts import (
     PROMPT_ESTIMATOR,
     PROMPT_LINEAR,
     PROMPT_NAME,
+    PROMPT_OPTIONAL,
+    PROMPT_OUTPUT,
     PROMPT_PREDICT_PROBA,
+    PROMPT_REQUIRED,
+    PROMPT_SAMPLE_WEIGHT,
+    PROMPT_TAGS,
 )
 from sksmithy.cli import app
 
@@ -24,50 +29,49 @@ def test_version() -> None:
 
 
 @pytest.mark.parametrize("linear", ["y", "N"])
-def test_forge_default(tmp_path: Path, name: str, estimator: EstimatorType, linear: str) -> None:
+def test_forge_estimator(tmp_path: Path, name: str, estimator: EstimatorType, linear: str) -> None:
     """Tests that prompts are correct for classifier estimator."""
 
+    output_file = tmp_path / (f"{name.lower()}.py")
     _input = "".join(
         [
             f"{name}\n",  # name
             f"{estimator.value}\n",  # estimator_type
+            "\n",  # required params
+            "\n",  # optional params
+            "\n",  # sample weight
             f"{linear}\n" if estimator in {EstimatorType.ClassifierMixin, EstimatorType.RegressorMixin} else "",
             "\n" if estimator in {EstimatorType.ClassifierMixin, EstimatorType.OutlierMixin} else "",  # predict_proba
             "\n"
             if (linear == "N" and estimator == EstimatorType.ClassifierMixin)
-            else "",  # decision_function: prompted only if linear is False
+            else "",  # decision_function: prompted only if not linear
+            "\n",  # tags
+            f"{output_file!s}\n",  # output file
         ]
     )
 
-    output_file = tmp_path / (f"{name.lower()}.py")
-
-    # fmt: off
     result = runner.invoke(
         app=app,
-        # By specifying arguments here it is easier to control for the others above.
-        # However, this means that prompts are not appearing in the console.
-        args=[
-            "forge",
-            "--required-params", "",
-            "--optional-params", "",
-            "--no-sample-weight",
-            "--tags", "",
-            "--output-file", str(output_file)
-        ],
+        args=["forge"],
         input=_input,
     )
-    # fmt: on
 
     assert result.exit_code == 0
 
     # General prompts
-    assert PROMPT_NAME in result.stdout
-    assert PROMPT_ESTIMATOR in result.stdout
-    # assert PROMPT_REQUIRED in result.stdout
-    # assert PROMPT_OPTIONAL in result.stdout
-    # assert PROMPT_SAMPLE_WEIGHT in result.stdout
-    # assert PROMPT_TAGS in result.stdout
-    # assert f"{PROMPT_OUTPUT} [{name.lower()}.py]" in result.stdout
+    assert all(
+        _prompt in result.stdout
+        for _prompt in (
+            PROMPT_NAME,
+            PROMPT_ESTIMATOR,
+            PROMPT_REQUIRED,
+            PROMPT_OPTIONAL,
+            PROMPT_SAMPLE_WEIGHT,
+            PROMPT_TAGS,
+            f"{PROMPT_OUTPUT} [{name.lower()}.py]",
+        )
+    )
+
     assert output_file.exists
 
     # Estimator type specific prompts
@@ -81,98 +85,68 @@ def test_forge_default(tmp_path: Path, name: str, estimator: EstimatorType, line
 
 
 @pytest.mark.parametrize(
-    ("invalid_name", "err_msg"),
+    ("invalid_name", "name_err_msg"),
     [
         ("class", "Error: `class` is a python reserved keyword!"),
         ("abc-xyz", "Error: `abc-xyz` is not a valid python class name!"),
     ],
 )
-def test_forge_invalid_name(tmp_path: Path, name: str, invalid_name: str, err_msg: str) -> None:
-    """Tests that error messages are raised with invalid names."""
-
-    output_file = tmp_path / (f"{name.lower()}.py")
-    input_output_file = f"{output_file!s}\n"
-
-    _input = "".join(
-        [
-            f"{invalid_name}\n",  # name, invalid attempt
-            f"{name}\n",  # name, valid attempt
-            "transformer\n",  # estimator_type
-            "\n",  # required params
-            "\n",  # optional params
-            "\n",  # sample_weight
-            "",  # linear
-            "",  # predict_proba
-            "",  # decision_function
-            "\n",  # tags
-            input_output_file,  # output file
-        ]
-    )
-
-    result = runner.invoke(app, ["forge"], input=_input)
-
-    assert result.exit_code == 0
-    assert err_msg in result.stdout
-
-
 @pytest.mark.parametrize(
-    ("invalid_required", "err_msg"),
+    ("invalid_required", "required_err_msg"),
     [
         ("a-b", "Error: The following parameters are invalid python identifiers: ('a-b',)"),
         ("a,a", "Error: Found repeated parameters!"),
     ],
 )
-def test_forge_invalid_params(tmp_path: Path, name: str, invalid_required: str, err_msg: str) -> None:
-    """Tests that error messages are raised with invalid parameters."""
+@pytest.mark.parametrize(
+    ("invalid_optional", "dupliclated_err_msg"),
+    [("a", "Error: The following parameters are duplicated between required and optional: {'a'}")],
+)
+@pytest.mark.parametrize(
+    ("invalid_tags", "tags_err_msg"),
+    [("not-a-tag,also-not-a-tag", "Error: The following tags are not available: ('not-a-tag', 'also-not-a-tag').")],
+)
+def test_forge_invalid_args(
+    tmp_path: Path,
+    name: str,
+    invalid_name: str,
+    name_err_msg: str,
+    invalid_required: str,
+    required_err_msg: str,
+    invalid_optional: str,
+    dupliclated_err_msg: str,
+    invalid_tags: str,
+    tags_err_msg: str,
+) -> None:
+    """Tests that error messages are raised with invalid names."""
 
     output_file = tmp_path / (f"{name.lower()}.py")
-    input_output_file = f"{output_file!s}\n"
 
     _input = "".join(
         [
-            f"{name}\n",  # name
-            "transformer\n",  # estimator_type
-            f"{invalid_required}\n",  # required param, invalid attempt
-            "a,b\n",  # required params
-            "\n",  # optional params
-            "\n",  # sample_weight
-            "",  # linear
-            "",  # predict_proba
-            "",  # decision_function
-            "\n",  # tags
-            input_output_file,  # output file
-        ]
-    )
-
-    result = runner.invoke(app, ["forge"], input=_input)
-
-    assert result.exit_code == 0
-    assert err_msg in result.stdout
-
-
-def test_forge_duplicated_params(tmp_path: Path, name: str) -> None:
-    """Tests that error messages are raised with duplicated parameters."""
-
-    output_file = tmp_path / (f"{name.lower()}.py")
-    input_output_file = f"{output_file!s}\n"
-
-    _input = "".join(
-        [
-            f"{name}\n",  # name
-            "transformer\n",  # estimator_type
-            "a,b\n",  # required params
-            "a\n",  # optional params, invalid attempt
+            f"{invalid_name}\n",  # name, invalid attempt
+            f"{name}\n",  # name, valid attempt
+            "transformer\n"  # type
+            f"{invalid_required}\n",  # required params, invalid attempt
+            "a,b\n",  # required params, valid attempt
+            f"{invalid_optional}\n",  # optional params, invalid attempt
             "c,d\n",  # optional params, valid attempt
             "\n",  # sample_weight
-            "",  # linear
-            "",  # predict_proba
-            "",  # decision_function
-            "\n",  # tags
-            input_output_file,  # output file
+            f"{invalid_tags}\n",  # tags, invalid attempt
+            "binary_only\n",  # valid attempt
+            f"{output_file!s}\n",
         ]
+    )
+
+    result = runner.invoke(
+        app=app,
+        args=["forge"],
+        input=_input,
     )
 
     result = runner.invoke(app, ["forge"], input=_input)
 
     assert result.exit_code == 0
-    assert "Error: The following parameters are duplicated between required and optional: {'a'}" in result.stdout
+    assert all(
+        err_msg in result.stdout for err_msg in (name_err_msg, required_err_msg, dupliclated_err_msg, tags_err_msg)
+    )
